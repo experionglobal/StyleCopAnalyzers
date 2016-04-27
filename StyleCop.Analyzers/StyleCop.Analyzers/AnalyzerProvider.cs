829c4794e4f5d88819ee0afc5c7768456c53ba4b
Benoy Bose <benoybose@experionglobal.com>
2016-04-27 16:54:18 +0530

    Added provider to get analyzers from StyleCop

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/AnalyzerProvider.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace StyleCop.Analyzers
+{
+    /// <summary>
+    /// Provide list of analyzers
+    /// </summary>
+    public static class AnalyzerProvider
+    {
+        /// <summary>
+        /// Discover all analyzers using reflection
+        /// </summary>
+        /// <returns>Enumerable of all analyzer objects</returns>
+        public static IEnumerable<DiagnosticAnalyzer> GetAnalyzers()
+        {
+            var assembly = typeof(AnalyzerProvider).GetTypeInfo().Assembly;
+            var analyzers = assembly.DefinedTypes.Where(t => t.BaseType == typeof(DiagnosticAnalyzer))
+                .Where(y => !y.IsAbstract)
+                .Select(x => (DiagnosticAnalyzer) Activator.CreateInstance(x.AsType()));
+            return analyzers.AsEnumerable();
+        }
+    }
+}
